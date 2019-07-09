f317437db56746eafbbed0046e5237e9ccd8e118
SamuxD <SamuxD@users.noreply.github.com>
2019-07-09 13:36:30 +0200

    Add files via upload

--- papiLights.ino
+++ papiLights.ino
@@ -7,6 +7,7 @@ void setup() {
     MFS.initialize(&Timer1);
 }
 
+//set initial height
 int lossOfFeetPerMinute = 100;
 
 void loop() {
@@ -34,17 +35,19 @@ void loop() {
           lossOfFeetPerMinute = 100;
         }
       }
+      }
 
       
-
+//check if the plane is too low if thats the case sound warning noise + let all 4 leds shine in red
     if(lossOfFeetPerMinute < 50)
     {
       MFS.writeLeds(LED_1, ON);
       MFS.writeLeds(LED_2, ON);
       MFS.writeLeds(LED_3, ON);
       MFS.writeLeds(LED_4, ON);
-      MFS.beep(1);
+      MFS.beep(3);
     }
+//check if the plane is just slightly to low if so turn on 3 of 4 leds 
     if(lossOfFeetPerMinute > 50 && lossOfFeetPerMinute < 75)
     {
       MFS.writeLeds(LED_1, OFF);
@@ -53,6 +56,7 @@ void loop() {
       MFS.writeLeds(LED_4, ON);
       MFS.beep(OFF);
     }
+//check if the plane is on a suitable height turn off 2 leds to represent 2 white and let 2 turned on so that youve got 2 "white" and 2 red to indicate that everything is fine
     if(lossOfFeetPerMinute > 75 && lossOfFeetPerMinute < 125)
     {
       MFS.writeLeds(LED_1, OFF);
@@ -60,6 +64,7 @@ void loop() {
       MFS.writeLeds(LED_3, ON);
       MFS.writeLeds(LED_4, ON);
     }
+//check if the plane is slightly to high if so show 3 white 1 red
     if(lossOfFeetPerMinute > 125 && lossOfFeetPerMinute < 150)
     {
       MFS.writeLeds(LED_1, OFF);
@@ -67,6 +72,7 @@ void loop() {
       MFS.writeLeds(LED_3, OFF);
       MFS.writeLeds(LED_4, ON);
     }
+//check if user is way to high turn off all leds to represent 4 white which indicates that the plane is way heigher than its supposed to be
     if(lossOfFeetPerMinute > 150 && lossOfFeetPerMinute < 200)
     {
       MFS.writeLeds(LED_1, OFF);
@@ -74,5 +80,5 @@ void loop() {
       MFS.writeLeds(LED_3, OFF);
       MFS.writeLeds(LED_4, OFF);
     }
-}
+
 }
